5396bbbdaf8f7a774e003d4e7230be630bc3712b
zoirs <zoirs@ya.ru>
2019-07-28 11:08:20 +0300

    Initial commit

---
+++ src/main/java/ru/chernyshev/yandexspeechkit/YandexSpeechkitApplication.java
@@ -0,0 +1,13 @@
+package ru.chernyshev.yandexspeechkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexSpeechkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandexSpeechkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/chernyshev/yandexspeechkit/YandexSpeechkitApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.chernyshev.yandexspeechkit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YandexSpeechkitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
