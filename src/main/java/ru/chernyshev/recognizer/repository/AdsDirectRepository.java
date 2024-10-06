36e035e1cb24de7a8f48ca376c9c08d46a92bc51
Denis <zoirs@ya.ru>
2024-10-06 12:29:40 +0300

    query fix

--- src/main/java/ru/chernyshev/recognizer/repository/AdsDirectRepository.java
+++ src/main/java/ru/chernyshev/recognizer/repository/AdsDirectRepository.java
@@ -13,7 +13,7 @@ public interface AdsDirectRepository extends CrudRepository<AdsSended, Long> {
 
     @Query(value = "select s from AdsSended s where s.adsId not in (select id from AdsEntity where type != 'DIRECT' or testChatId is not null) order by id desc")
     List<AdsSended> getLastDirectSended(Pageable pageable);
-    @Query(value = "select s from AdsSended s where s.adsId = :adsID and chatId in :chatIds")
+    @Query(value = "select s from AdsSended s where s.adsId = :adsID and chatId in :chatIds and isSuccess = true")
     List<AdsSended> getResult(@Param("adsID")Long adsId, @Param("chatIds") Set<Long> chatIds);
     AdsSended findFirstByAdsIdOrderByIdAsc(Long adsId);
     Long countByAdsId(Long adsId);
